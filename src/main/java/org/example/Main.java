3ab02c4f747fe1e177304542dbc81ca33fd1575b
onlyj <tlawltjr23@gmail.com>
2024-03-15 16:47:13 +0900

    240315 spring plugin 추가2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
-import org.example.module.CrudAuto;
 
-public class Main extends CrudAuto {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 
 }
