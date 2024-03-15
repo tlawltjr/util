a19df69546cc044dd27dfc825bd35900a9a683e7
onlyj <tlawltjr23@gmail.com>
2024-03-15 17:05:31 +0900

    240315 spring 제거 rollback

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
 
+import org.example.module.CrudAuto;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("hello world");
     }
 
