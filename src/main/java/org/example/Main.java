15f913f1556c4f1483f626f7ef6a2399cbddeba0
onlyj <tlawltjr23@gmail.com>
2024-03-15 16:20:07 +0900

    240315 gradle 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,7 @@
 package org.example;
 
 import org.example.module.CrudAuto;
-import org.example.sample.Dto;
-import org.example.sample.Entity;
 
 public class Main extends CrudAuto {
 
-    public void aa(){
-        Entity entity = Entity.builder()
-                .name("js")
-                .age(30)
-                .build();
-
-        Dto dto = super.entityToDto(Dto.class, entity);
-
-        System.out.println(dto.toString());
-    }
-    public static void main(String[] args) {
-//        AddCommas ac = new AddCommas();
-//
-//        System.out.println(ac.addComma(1000000000L));
-    Main ma = new Main();
-
-    ma.aa();
-
-
-    }
 }
