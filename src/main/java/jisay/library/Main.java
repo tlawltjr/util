d4aa4815f60a4db8d470d74f2156e50b68d147b8
onlyj <tlawltjr23@gmail.com>
2024-03-19 15:00:10 +0900

    240319 main 수정

--- src/main/java/jisay/library/Main.java
+++ src/main/java/jisay/library/Main.java
@@ -7,9 +7,6 @@ import jisay.library.module.CrudAuto;
 public class Main {
     public static void main(String[] args) {
 
-        CrudAuto ca = new CrudAuto(new Func() {
-        });
-
         System.out.println("hello world");
     }
 
